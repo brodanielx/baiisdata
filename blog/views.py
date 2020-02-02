06998ed499016c04e0684e7660ff5935759fec6e
daniel <bro.danielx@gmail.com>
2020-02-02 07:36:04 -0500

    templates intro

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Post 1',
+        'content': 'first post content',
+        'date_posted': '10/9/2019'
+    },
+    {
+        'author': 'Daniel X',
+        'title': 'Post 2',
+        'content': 'second post content',
+        'date_posted': '10/11/2019'
+    },
+]
+
+
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+
+    context = {
+        'title': 'About'
+    }
+
+    return render(request, 'blog/about.html', context)
 
